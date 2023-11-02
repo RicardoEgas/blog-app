8b5c4a43e4d8df2c6a34df86bdc2ee178d49d86c
RicardoEgas <rj_egas@hotmail.com>
2023-11-02 18:35:47 +0100

    Fix Linter errors

--- test/models/new_post_test.rb
+++ test/models/new_post_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class NewPostTest < ActiveSupport::TestCase
   # test "the truth" do
--- test/models/new_user_test.rb
+++ test/models/new_user_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class NewUserTest < ActiveSupport::TestCase
   # test "the truth" do
