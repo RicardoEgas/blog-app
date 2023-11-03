e4fd2596ab5274b77ce8f73e9856bec12e7bf26e
RicardoEgas <rj_egas@hotmail.com>
2023-11-03 17:21:43 +0100

    Correct test

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  before :all do
-    @author = User.create(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                          bio: 'Teacher from Poland.')
-    @user = User.create(name: 'David Beckham', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                        bio: 'Teacher from Poland.')
-    @post = Post.create(author: @author, title: 'Hello World', text: 'I love coding', comments_counter: 0,
-                        likes_counter: 0)
-  end
-
-  context '#update_likes_counter' do
+  describe '#update_likes_counter' do
     before :all do
+      @user = User.create(name: 'David Beckham', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                          bio: 'Teacher from Poland.')
+      @post = Post.create(author: @user, title: 'Hello World', text: 'I love coding', comments_counter: 0,
+                          likes_counter: 0)
+    end
+
+    before do
       10.times { Like.create(user: @user, post: @post) }
     end
 
     it 'keeps track of likes and equals 10' do
-      expect(@post.likes_counter).to eq 10
+      expect(@post.reload.likes_counter).to eq 10
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,4 @@
+# user_spec.rb
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
@@ -12,17 +13,21 @@ RSpec.describe User, type: :model do
   end
 
   describe '#recent_posts' do
-    it 'returns the 3 most recent posts' do
-      user = User.create(name: 'John Doe', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                         bio: 'Teacher from Poland.')
-      post1 = Post.create(author: user, title: 'Post 1', text: 'hello world', comments_counter: 0,
-                          likes_counter: 0, created_at: 2.days.ago)
-      post2 = Post.create(author: user, title: 'Post 2', text: 'hello world', comments_counter: 0,
-                          likes_counter: 0, created_at: 2.days.ago)
-      post3 = Post.create(author: user, title: 'Post 3', text: 'hello world', comments_counter: 0,
-                          likes_counter: 0, created_at: 2.days.ago)
+    it 'returns the 2 most recent posts' do
+      user = User.create(name: 'Example User', photo: 'example.jpg', bio: 'Example bio')
+      post1 = Post.create(author: user, title: 'Post 1', text: 'hello world')
+      post2 = Post.create(author: user, title: 'Post 2', text: 'hello world')
+      post3 = Post.create(author: user, title: 'Post 3', text: 'hello world')
+  
       rec_posts = user.recent_posts
-      expect(rec_posts).to eq([post3, post2, post1])
+      rec_posts_array = rec_posts.to_a
+  
+      expected_posts = [post3, post2].sort_by(&:created_at)
+      actual_posts = rec_posts_array.sort_by(&:created_at)
+  
+      expect(actual_posts).to eq(expected_posts)
     end
   end
+  
+  
 end
