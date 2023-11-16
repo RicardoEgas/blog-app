dde7b77bdaba6e031e072729f44ec9830322e03c
RicardoEgas <rj_egas@hotmail.com>
2023-11-16 15:47:02 +0100

    implement code to Destroy comments before deleting posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,16 +26,20 @@ class PostsController < ApplicationController
     end
   end
 
-  def destroy
-    @post = Post.includes(:likes).find(params[:id])
-    @author = @post.author
-    @author.decrement!(:post_counter)
-    @post.likes.destroy_all
-    @post.destroy!
+# app/controllers/posts_controller.rb
 
-    redirect_to user_posts_path(id: @author.id), notice: 'Post successfully deleted'
-  end
+# app/controllers/posts_controller.rb
 
+def destroy
+  @post = Post.includes(:likes).find(params[:id])
+  Comment.where(post_id: @post.id).destroy_all
+  @author = @post.author
+  @author.decrement!(:post_counter)
+  @post.likes.destroy_all
+  @post.destroy
+
+  redirect_to user_posts_path(id: @author.id), notice: 'Post successfully deleted'
+end
 
   private
 
