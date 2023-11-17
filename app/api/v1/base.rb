26176242d891e5dd0c7c503ce180022185ce0382
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-11-17 16:50:28 +0330

    Add post endpoint for comment

--- app/api/v1/base.rb
+++ app/api/v1/base.rb
@@ -1,28 +1,39 @@
 module V1
-    class Base < Grape::API
-      version 'v1', using: :path
-      format :json
-  
-      resource :users do
-        params do
-          requires :user_id, type: Integer
+  class Base < Grape::API
+    version 'v1', using: :path
+    format :json
+    resource :users do
+      params do
+        requires :user_id, type: Integer
+      end
+      route_param :user_id do
+        desc 'List all posts for a user'
+        get '/posts' do
+          user = User.find(params[:user_id])
+          user.posts
         end
-  
-        route_param :user_id do
-          desc 'List all posts for a user'
-          get '/posts' do
-            user = User.find(params[:user_id])
-            user.posts
-          end
-  
-          desc 'List all comments for a user\'s post'
-          get '/posts/:post_id/comments' do
-            user = User.find(params[:user_id])
-            post = user.posts.find(params[:post_id])
-            post.comments
+        desc 'List all comments for a user\'s post'
+        get '/posts/:post_id/comments' do
+          user = User.find(params[:user_id])
+          post = user.posts.find(params[:post_id])
+          post.comments
+        end
+        desc 'Add a comment to a post'
+        params do
+          requires :post_id, type: Integer
+          requires :text, type: String
+        end
+        post '/posts/:post_id/comments' do
+          user = User.find(params[:user_id])
+          post = user.posts.find(params[:post_id])
+          comment = post.comments.build(author: user, text: params[:text])
+          if comment.save
+            comment
+          else
+            error!('Comment could not be created', 422)
           end
         end
       end
     end
   end
-  
+end
